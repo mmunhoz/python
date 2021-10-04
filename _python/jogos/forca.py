3cc2256eb25d97041e2a1e0c4ea6f886bcec6ce6
Marcos Munhoz <marcosmunhozf@gmail.com>
2021-10-04 16:16:13 -0300

    fix directories

--- _python/jogos/forca.py
+++
@@ -1,7 +0,0 @@
-def jogar():
-    print("*******************************")
-    print("Bem vindo ao jogo de forca")
-    print("*******************************")
-
-if(__name__ == "__main__"):
-    jogar()
