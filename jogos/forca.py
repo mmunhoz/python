2a53c5048381b726d8188dfe676a6e0f659f5567
Marcos Munhoz <marcosmunhozf@gmail.com>
2021-10-04 15:25:56 -0300

    set of games in python

---
+++ jogos/forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("*******************************")
+    print("Bem vindo ao jogo de forca")
+    print("*******************************")
+
+    # todo: criar jogo
+
+if(__name__ == "__main__"):
+    jogar()
